3aa14b721fd9e1b61f7c08c01be3942a2b3b1aa8
Muhammad Shahid Rafi C P - MacBook Pro <muhammadshahidraficp@gmail.com>
2022-12-03 23:31:03 +0400

    FIX:edit feature added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import  {useState, useEffect} from 'react';
 import {Button,TextField} from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import EditIcon from '@material-ui/icons/Edit';
 import { updateDoc,setDoc,query,doc,collection, getDocs,addDoc,deleteDoc,serverTimestamp } from 'firebase/firestore';
 import {ListItem, List, ListItemText} from '@material-ui/core'
 
@@ -13,6 +14,9 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [editTodoData,setEditTodo] = useState("");
+  const [newTodoDataAfterEdit,setAfterEditTodo] = useState("");
   const collectionName = 'todo_list';
   const collectionRef = collection(db,collectionName);
 
@@ -38,6 +42,11 @@ function App() {
     setIsLoading(false);
   }
 
+  async function edit(todo){
+    setIsEditOpen(true);
+    setEditTodo(todo);
+  }
+
   const handleAddTodo = (event)=> {
       event.preventDefault();
       setIsLoading(true);
@@ -52,25 +61,40 @@ function App() {
     post();
   }
 
+  async function update(id,newTitle){
+    console.log("update")
+    const docRef = doc(db,collectionName, id);
+    await updateDoc(docRef,{updated:true,title:newTitle});
+    setIsEditOpen(false);
+    fetchData();
+  }
+
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <form>
-        {/* <InputLabel htmlFor="txt_todo" >New todo</InputLabel> */}
+      {isEditOpen && <div>
+        <TextField type="text"  variant="outlined" size='small' placeholder={editTodoData.item.title}
+         style={{marginRight:10}} onChange={(event)=>{setAfterEditTodo(event.target.value)}}/>
+        <Button variant='outlined' onClick={()=>{
+          update(editTodoData.id,newTodoDataAfterEdit);
+        }}>Edit</Button>
+      </div>}
+      {!isEditOpen&&<form>
         <TextField type="text" id="txt_todo" value={newTodo} label="Enter todo here" variant="outlined" size='small'
         onChange={event=>setNewTodo(event.target.value)} style={{marginRight:10}}/>
         <Button variant='outlined' color="secondary" type="submit" onClick={handleAddTodo} disabled={!newTodo}>Add todo</Button>
         {isLoading && <div>Loading....</div>}
         {!isLoading && <ul>
         {todos.map(todo =>{
-          return <List key={todo.id} className="todo_list">
+          return <List key={todo.id} className="todo_list" >
             <ListItem>
              <ListItemText primary={todo.item.title} secondary={todo.id}/>
             </ListItem>
+            <EditIcon onClick={()=>{edit(todo);}}/>
             <DeleteForeverIcon onClick={()=>{deleteData(todo.id)}}/>
           </List>})}
         </ul>}
-      </form>
+      </form>}
     </div>
   );
 }
